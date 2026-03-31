37cc68465f3e0ab522f26d8181693accc14a7715
Dylan Nguyen <nguyendylan164@gmail.com>
2026-03-31 00:22:49 -0500

    Implement SVM model and training script for image classification

---
+++ svm.py
@@ -0,0 +1,30 @@
+# SVM model built from scratch
+import numpy as np
+
+class SVM:
+    def __init__(self, learning_rate=0.001, lambda_param=0.01, iterations=1000):
+        self.lr = learning_rate
+        self.n_itr = iterations
+        self.lambda_param = lambda_param
+        self.b = 0
+        self.w = None
+    
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        y_ = np.where(y <= 0, -1, 1)
+        self.w = np.zeros(n_features)
+        
+        # gradient descent
+        for i in range(self.n_itr):
+            for idx, x_i in enumerate(X):
+                condition = y_[idx] * (np.dot(x_i, self.w) - self.b) >= 1
+                if condition:
+                    self.w -= self.lr * (2 * self.lambda_param * self.w)
+                else:
+                    self.w -= self.lr * (2 * self.lambda_param * self.w - np.dot(x_i, y_[idx]))
+                    self.b -= self.lr * y_[idx]
+
+    def predict(self, X):
+        approx = np.dot(X, self.w) - self.b
+        return np.where(np.sign(approx) == -1, 0, 1)
+        
---
+++ train_svm.py
@@ -0,0 +1,34 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import joblib
+
+from svm import SVM
+
+def train_svm_model():
+    X=np.load("X_features.npy")
+    y=np.load("y_labels.npy")
+
+    X_no_color = X[:, 1:]
+
+    X_train, X_test, y_train, y_test = train_test_split(X_no_color, y, test_size=0.2, random_state=42)
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    svm_model = SVM(learning_rate=0.00001, lambda_param=0.3, iterations=1000)
+    svm_model.fit(X_train, y_train)
+
+    joblib.dump(svm_model, "svm_model.joblib")
+    joblib.dump(scaler, "scaler.joblib")
+
+    y_pred = svm_model.predict(X_test)
+    print(f"Accuracy: {accuracy_score(y_test, y_pred) * 100:.3f}\n")
+    print("Classification Report:")
+    print(classification_report(y_test, y_pred, target_names=["Real Art", "AI Art"]) + "\n")
+    print("Confusion Matrix:")
+    print(confusion_matrix(y_test, y_pred))
+
+train_svm_model()
